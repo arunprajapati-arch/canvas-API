e483f0ca6688b7d061f49b528501754355096cba
arunprajapati-arch <arunprajapati.mail@gmail.com>
2024-08-13 20:35:13 +0530

    drawing complete

--- index.js
+++ index.js
@@ -6,11 +6,32 @@ window.addEventListener("load", () => {
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
     
-    ctx.beginPath();
-    ctx.moveTo(100, 100);
-    ctx.lineTo(200, 100);
-    ctx.lineTo(200, 150);
-    ctx.closePath();
-    ctx.stroke();
+    let painting = false;
     
+    function startPosition(e){
+        painting = true;
+        draw(e);
+    }
+
+    function finishedPosition(){
+        painting = false;
+        ctx.beginPath();
+    }
+
+    function draw(e){
+        if (!painting) return;
+        ctx.lineWidth = 10;
+        ctx.lineCap = "round";
+
+        ctx.lineTo(e.clientX, e.clientY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY)
+    }
+
+
+
+    canvas.addEventListener("mousedown", startPosition);
+    canvas.addEventListener("mouseup", finishedPosition);
+    canvas.addEventListener("mousemove", draw);
 });
